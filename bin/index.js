e5a7be2bfea27126aada488a611e7a99b2a84758
Mike Shukshin <merqrylive@gmail.com>
2018-03-26 16:42:31 +0300

    Some small fixes and remove console.log

--- bin/index.js
+++ bin/index.js
@@ -22,28 +22,24 @@ const spawn = require('react-dev-utils/crossSpawn');
 //   case 'eject':
 //   case 'start':
 //   case 'test': {
-    const result = spawn.sync(
-      'node',
-      [require.resolve('./dev.js')],
-      { stdio: 'inherit' }
+const result = spawn.sync('node', [require.resolve('./dev.js')], { stdio: 'inherit' });
+if (result.signal) {
+  if (result.signal === 'SIGKILL') {
+    console.log(
+      'The build failed because the process exited too early. ' +
+        'This probably means the system ran out of memory or someone called ' +
+        '`kill -9` on the process.'
     );
-    if (result.signal) {
-      if (result.signal === 'SIGKILL') {
-        console.log(
-          'The build failed because the process exited too early. ' +
-            'This probably means the system ran out of memory or someone called ' +
-            '`kill -9` on the process.'
-        );
-      } else if (result.signal === 'SIGTERM') {
-        console.log(
-          'The build failed because the process exited too early. ' +
-            'Someone might have called `kill` or `killall`, or the system could ' +
-            'be shutting down.'
-        );
-      }
-      process.exit(1);
-    }
-    process.exit(result.status);
+  } else if (result.signal === 'SIGTERM') {
+    console.log(
+      'The build failed because the process exited too early. ' +
+        'Someone might have called `kill` or `killall`, or the system could ' +
+        'be shutting down.'
+    );
+  }
+  process.exit(1);
+}
+process.exit(result.status);
 //     break;
 //   }
 //   default:
