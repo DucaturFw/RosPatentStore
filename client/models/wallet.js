5305f785c709e203d30448d13748a757e4ffe4de
Mikhail Shukshin <merqrylive@gmail.com>
2018-04-05 19:33:38 +0300

    fix start without web3

--- client/models/wallet.js
+++ client/models/wallet.js
@@ -4,12 +4,18 @@ const JOIN_ADDR = '0xad2870197485a3f0ecbfdbd9de54ab90905df2de';
 
 class Wallet {
   constructor() {
-    // eslint-disable-next-line
-    this.web3 = new Web3(web3.currentProvider);
+    if (typeof web3 !== 'undefined') {
+      // eslint-disable-next-line
+      this.web3 = new Web3(web3.currentProvider);
+    }
+  }
+
+  isActive() {
+    return !!this.web3;
   }
 
   getAddr() {
-    if (this.web3.eth.accounts && this.web3.eth.accounts.length > 0) {
+    if (this.isActive() && this.web3.eth.accounts && this.web3.eth.accounts.length > 0) {
       return this.web3.eth.accounts[0];
     }
   }
@@ -19,20 +25,22 @@ class Wallet {
   }
 
   send(amount) {
-    let tx = {
-      from: this.getAddr(),
-      to: JOIN_ADDR,
-      value: this.web3.toWei(amount)
-    }
+    if (this.isActive()) {
+      let tx = {
+        from: this.getAddr(),
+        to: JOIN_ADDR,
+        value: this.web3.toWei(amount)
+      }
 
-    if (this.hasAddr()) {
-      this.web3.eth.sendTransaction(tx,
-        function (err, transactionHash) {
-          if (!err) {
-            console.log('join done', transactionHash);
+      if (this.hasAddr()) {
+        this.web3.eth.sendTransaction(tx,
+          function (err, transactionHash) {
+            if (!err) {
+              console.log('join done', transactionHash);
+            }
           }
-        }
-      );
+        );
+      }
     }
   }
 }
